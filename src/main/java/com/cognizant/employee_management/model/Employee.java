0903c805d6f5775433267063aee9c94a85019675
SravaniCh-31 <Chunduri.Sravani@cognizant.com>
2025-04-03 14:29:22 +0530

    employee entity is created

--- src/main/java/com/cognizant/employee_management/model/Employee.java
+++ src/main/java/com/cognizant/employee_management/model/Employee.java
@@ -1,5 +1,19 @@
 package com.cognizant.employee_management.model;
 
-public class Employee {
+import java.time.LocalDate;
+import lombok.Data;
 
+@Data
+public class Employee {
+	private int employeeId;
+	private int managerId;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private String department;
+	private String role;
+	private LocalDate joinedDate;
 }
