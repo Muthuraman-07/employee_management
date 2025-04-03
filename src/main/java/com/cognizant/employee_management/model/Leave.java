16bb697756e0d099354ed773f5acf4405c742024
Elanthiraivel.K <Elanthiraivel.K@cognizant.com>
2025-04-03 14:11:56 +0530

    leave entity is created

--- src/main/java/com/cognizant/employee_management/model/Leave.java
+++ src/main/java/com/cognizant/employee_management/model/Leave.java
@@ -1,5 +1,14 @@
 package com.cognizant.employee_management.model;
-
+import java.util.Date;
+import lombok.Data;
+@Data
 public class Leave {
-
+	private int leaveId;
+	private int employeeId;
+	private Date appliedDate;
+	private String leaveType;
+	private Date startDate;
+	private Date endDate;
+	private String status;
+	private Date approvedDate;
 }
