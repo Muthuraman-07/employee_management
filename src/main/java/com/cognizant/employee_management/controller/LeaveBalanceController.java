4722107f3e9d2b90d6d17d320dee7e5affe59f04
MeghanaAkondi <Akondi.Meghana@cognizant.com>
2025-04-16 17:52:29 +0530

    Shift model updated

--- src/main/java/com/cognizant/employee_management/controller/LeaveBalanceController.java
+++ src/main/java/com/cognizant/employee_management/controller/LeaveBalanceController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,33 +23,31 @@ import jakarta.validation.Valid;
 public class LeaveBalanceController {
 
 	@Autowired
-    private LeaveBalanceService leaveBalanceService;
- 
-    @PostMapping
-    public LeaveBalanceDto create(@Valid @RequestBody LeaveBalanceDto dto) {
-        return leaveBalanceService.createLeaveBalance(dto);
-    }
- 
-    @GetMapping("/{id}")
-    public LeaveBalanceDto getById(@PathVariable int id) {
-        return leaveBalanceService.getLeaveBalanceById(id);
-    }
- 
-    @GetMapping("/getAll")
-    public List<LeaveBalanceDto> getAll() {
-        return leaveBalanceService.getAllLeaveBalances();
-    }
- 
-    @PutMapping("/{id}")
-    public LeaveBalanceDto update(@PathVariable int id,@Valid @RequestBody LeaveBalanceDto dto) {
-        return leaveBalanceService.updateLeaveBalance(id, dto);
-    }
- 
-    
- 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable int id) {
-        leaveBalanceService.deleteLeaveBalance(id);
-        return ResponseEntity.ok("LeaveBalance deleted successfully.");
-    }
+	private LeaveBalanceService leaveBalanceService;
+
+	@PostMapping
+	public LeaveBalanceDto create(@Valid @RequestBody LeaveBalanceDto dto) {
+		return leaveBalanceService.createLeaveBalance(dto);
+	}
+
+	@GetMapping("/{id}")
+	public LeaveBalanceDto getById(@PathVariable int id) {
+		return leaveBalanceService.getLeaveBalanceById(id);
+	}
+
+	@GetMapping("/getAll")
+	public List<LeaveBalanceDto> getAll() {
+		return leaveBalanceService.getAllLeaveBalances();
+	}
+
+	@PutMapping("/{id}")
+	public LeaveBalanceDto update(@PathVariable int id, @Valid @RequestBody LeaveBalanceDto dto) {
+		return leaveBalanceService.updateLeaveBalance(id, dto);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> delete(@PathVariable int id) {
+		leaveBalanceService.deleteLeaveBalance(id);
+		return ResponseEntity.ok("LeaveBalance deleted successfully.");
+	}
 }
--- src/main/java/com/cognizant/employee_management/dto/ShiftDto.java
+++ src/main/java/com/cognizant/employee_management/dto/ShiftDto.java
@@ -20,36 +20,5 @@ public class ShiftDto {
     @NotNull(message = "Shift End Time cannot be null")
     private LocalTime shiftEndTime;
  
-    // Getters and Setters
-    public int getShiftId() {
-        return shiftId;
-    }
- 
-    public void setShiftId(int shiftId) {
-        this.shiftId = shiftId;
-    }
- 
-    public LocalDate getShiftDate() {
-        return shiftDate;
-    }
- 
-    public void setShiftDate(LocalDate shiftDate) {
-        this.shiftDate = shiftDate;
-    }
- 
-    public LocalTime getShiftStartTime() {
-        return shiftStartTime;
-    }
- 
-    public void setShiftStartTime(LocalTime shiftStartTime) {
-        this.shiftStartTime = shiftStartTime;
-    }
- 
-    public LocalTime getShiftEndTime() {
-        return shiftEndTime;
-    }
- 
-    public void setShiftEndTime(LocalTime shiftEndTime) {
-        this.shiftEndTime = shiftEndTime;
-    }
+   
 }
